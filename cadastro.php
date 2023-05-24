537d4e91dd726c79762c34f44625bbd10c317407
Matheus Juliao <matheusjosejuliao@gmail.com>
2023-05-23 21:58:03 -0300

    Tela de cadastrato - front

--- cadastro.php
+++ cadastro.php
@@ -1,35 +1,43 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Cadastro</title>
+        <link rel="stylesheet" href="css/login.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
 
-    <body style="display: flex; align-items: center; justify-content: center; background: linear-gradient(to top, #121212, #a3a2a2);">
+    <body>
         <div class="card" style="width: 18rem;">
             <div class="card-body">
-                <form method="POST" enctype="multipart/form-data" style="text-align: center;">
+            <form method="POST" enctype="multipart/form-data" style="text-align: center;">
                     <img src="./images/logo-biofitness.png" style="width: 150px;">
                     <table>
                         <tr>
                             <td>
-                                Username
+                                Nome
                                 <input type="text" name="user" style="width: 250px;">
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                password
-                                <input type="password" name="pass" style="width: 250px;">
+                                E-mail
+                                <input type="email" name="email" style="width: 250px;">
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <a href="reg.php">Não tem uma conta? Cadastre-se</a>
-                                <button type="submit" name="sub" value="submit" class="btn mt-3"
-                                    style="float: right; border-color:#1c2d50;">LOGIN</button>
+                                Senha
+                                <input type="password" name="password" style="width: 250px;">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button type="submit" name="submit" value="submit" class="btn mt-3"
+                                    style="float: right; border-color:#1c2d50;">CONFIRMAR</button>
+                                <a href="index.php"><div class="btn mt-3"
+                                    style="float: left; border-color:#1c2d50;">VOLTAR</div></a>
                             </td>
                         </tr>
                     </table>
@@ -37,9 +45,4 @@
             </div>
         </div>
     </body>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
-    </script>
-
 </html>
