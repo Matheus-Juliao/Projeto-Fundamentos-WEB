271dfd945c0c447a64f63c9ab493f3c184147640
Matheus Juliao <matheusjosejuliao@gmail.com>
2023-06-13 19:50:15 -0300

    Adjust export

--- screens/reports/students-plans.php
+++ screens/reports/students-plans.php
@@ -5,7 +5,7 @@
     $html = '';
     $html .= '<table border="1">';
         
-    $html .= '<td colspan="14">Alunos e Planos</td>';
+    $html .= '<td colspan="7">Alunos e Planos</td>';
         $html .=  '<tr>';
             $html .= '<td><b>Nome</b></td>';
             $html .= '<td><b>Idade</b></td>';
@@ -40,7 +40,7 @@
     header ("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
     header ("Cache-Control: no-cache, must-revalidate");
     header ("Pragma: no-cache");
-    header ("Content-type: application/x-msexcel");
+    header("Content-type: text/csv; charset=utf-8");
     header ("Content-Disposition: attachment; filename=\"{$arquivo}\"" );
     header ("Content-Description: PHP Generated Data" );
     // Envia o conteúdo do arquivo
